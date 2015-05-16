6af3cd46d6c7019cee391eacde04bb6da6d2266a
khpolash <khasan994@gmail.com>
2015-05-17 01:11:31 +0600

    Initial CityInformationApp

---
+++ CityInformationApp/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace CityInformationApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.countryTextBox = new System.Windows.Forms.TextBox();
+            this.aboutTextBox = new System.Windows.Forms.TextBox();
+            this.cityNameTextBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.showListView = new System.Windows.Forms.ListView();
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.searchButton = new System.Windows.Forms.Button();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.searchByCountryRadioButton = new System.Windows.Forms.RadioButton();
+            this.searchByCityRadioButton = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.countryTextBox);
+            this.groupBox1.Controls.Add(this.aboutTextBox);
+            this.groupBox1.Controls.Add(this.cityNameTextBox);
+            this.groupBox1.Controls.Add(this.saveButton);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(153, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(396, 161);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "City Information";
+            // 
+            // countryTextBox
+            // 
+            this.countryTextBox.Location = new System.Drawing.Point(94, 104);
+            this.countryTextBox.Name = "countryTextBox";
+            this.countryTextBox.Size = new System.Drawing.Size(271, 20);
+            this.countryTextBox.TabIndex = 6;
+            // 
+            // aboutTextBox
+            // 
+            this.aboutTextBox.Location = new System.Drawing.Point(94, 45);
+            this.aboutTextBox.Multiline = true;
+            this.aboutTextBox.Name = "aboutTextBox";
+            this.aboutTextBox.Size = new System.Drawing.Size(271, 54);
+            this.aboutTextBox.TabIndex = 5;
+            // 
+            // cityNameTextBox
+            // 
+            this.cityNameTextBox.Location = new System.Drawing.Point(94, 20);
+            this.cityNameTextBox.Name = "cityNameTextBox";
+            this.cityNameTextBox.Size = new System.Drawing.Size(271, 20);
+            this.cityNameTextBox.TabIndex = 4;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(290, 129);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 3;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Country";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "About City";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "City Name";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.showListView);
+            this.groupBox2.Controls.Add(this.searchButton);
+            this.groupBox2.Controls.Add(this.searchTextBox);
+            this.groupBox2.Controls.Add(this.searchByCountryRadioButton);
+            this.groupBox2.Controls.Add(this.searchByCityRadioButton);
+            this.groupBox2.Location = new System.Drawing.Point(56, 179);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(624, 315);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Search";
+            // 
+            // showListView
+            // 
+            this.showListView.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.showListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader5,
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.showListView.FullRowSelect = true;
+            this.showListView.GridLines = true;
+            this.showListView.Location = new System.Drawing.Point(6, 57);
+            this.showListView.Name = "showListView";
+            this.showListView.Size = new System.Drawing.Size(612, 250);
+            this.showListView.TabIndex = 6;
+            this.showListView.UseCompatibleStateImageBehavior = false;
+            this.showListView.View = System.Windows.Forms.View.Details;
+            this.showListView.DoubleClick += new System.EventHandler(this.showListView_DoubleClick_1);
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "ID";
+            this.columnHeader5.Width = 0;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Serial No.";
+            this.columnHeader1.Width = 80;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "City Name";
+            this.columnHeader2.Width = 110;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "About City";
+            this.columnHeader3.Width = 300;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Country";
+            this.columnHeader4.Width = 130;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(468, 10);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 5;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(191, 12);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(271, 20);
+            this.searchTextBox.TabIndex = 4;
+            // 
+            // searchByCountryRadioButton
+            // 
+            this.searchByCountryRadioButton.AutoSize = true;
+            this.searchByCountryRadioButton.Location = new System.Drawing.Point(70, 36);
+            this.searchByCountryRadioButton.Name = "searchByCountryRadioButton";
+            this.searchByCountryRadioButton.Size = new System.Drawing.Size(112, 17);
+            this.searchByCountryRadioButton.TabIndex = 3;
+            this.searchByCountryRadioButton.TabStop = true;
+            this.searchByCountryRadioButton.Text = "Search by Country";
+            this.searchByCountryRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // searchByCityRadioButton
+            // 
+            this.searchByCityRadioButton.AutoSize = true;
+            this.searchByCityRadioButton.Checked = true;
+            this.searchByCityRadioButton.Location = new System.Drawing.Point(70, 13);
+            this.searchByCityRadioButton.Name = "searchByCityRadioButton";
+            this.searchByCityRadioButton.Size = new System.Drawing.Size(93, 17);
+            this.searchByCityRadioButton.TabIndex = 2;
+            this.searchByCityRadioButton.TabStop = true;
+            this.searchByCityRadioButton.Text = "Search by City";
+            this.searchByCityRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 498);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "City Information UI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox countryTextBox;
+        private System.Windows.Forms.TextBox aboutTextBox;
+        private System.Windows.Forms.TextBox cityNameTextBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton searchByCountryRadioButton;
+        private System.Windows.Forms.RadioButton searchByCityRadioButton;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.ListView showListView;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
+
---
+++ CityInformationApp/Form1.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CityInformationApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string connectionString = ConfigurationManager.ConnectionStrings["CityInformationConnString"].ConnectionString;
+
+        private bool isUpdateMode = false;
+        private int cityId;
+
+         
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            City city = new City();
+            city.cityName = cityNameTextBox.Text;
+            city.about = aboutTextBox.Text;
+            city.country = countryTextBox.Text;
+
+            if (isUpdateMode)
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                string insertQuery = "UPDATE cityInfoTable SET About='" + city.about + "',Country='" + city.country + "' WHERE ID='"+cityId+"'";
+                SqlCommand command = new SqlCommand(insertQuery, connection);
+                connection.Open();
+                int rowAffected = command.ExecuteNonQuery();
+                connection.Close();
+                if (rowAffected > 0)
+                {
+                    MessageBox.Show(@"City Information Successfully Update...!");
+                    isUpdateMode = false;
+                    saveButton.Text = "Save";
+                    cityId = 0;
+                    ShowAllData();
+                    cityNameTextBox.Clear();
+                    aboutTextBox.Clear();
+                    countryTextBox.Clear();
+                }
+                else
+                {
+                    MessageBox.Show(@"Update Failed...!");
+                }
+            }
+            else
+            {
+                if (city.cityName != "" && city.country != "")
+                {
+                    if (city.cityName.Length >= 4)
+                    {
+                        if (IsCityExists(city.cityName))
+                        {
+                            MessageBox.Show(@"City Name already Exists.");
+                            return;
+                        }
+                        SqlConnection connection = new SqlConnection(connectionString);
+
+                        string insertQuery = "INSERT INTO cityInfoTable VALUES('" + city.cityName + "','" + city.about + "','" + city.country + "')";
+
+                        SqlCommand command = new SqlCommand(insertQuery, connection);
+                        connection.Open();
+                        int rowAffected = command.ExecuteNonQuery();
+                        connection.Close();
+                        if (rowAffected > 0)
+                        {
+                            MessageBox.Show(@"City Information Successfully Inserted...!");
+                            ShowAllData();
+                        }
+                        else
+                        {
+                            MessageBox.Show(@"Isnertion Failed...!");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show(@"City Name Length at Least four(4) Characters");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show(@"Fill Up Requirement");
+                }
+            }
+            
+        }
+
+        public bool IsCityExists(string cityName)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            string selectQuery = "SELECT * FROM cityInfoTable WHERE CityName='"+cityName+"'";
+
+            SqlCommand command = new SqlCommand(selectQuery, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+
+            bool isCityExists = false;
+            while (reader.Read())
+            {
+                isCityExists = true;
+            }
+            reader.Close();
+            connection.Close();
+            return isCityExists;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ShowAllData();
+        }
+        public void ShowAllData()
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            string selectQuery = "SELECT * FROM cityInfoTable";
+
+            SqlCommand command = new SqlCommand(selectQuery, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+
+            List<City> cityList = new List<City>();
+
+            while (reader.Read())
+            {
+                City city = new City();
+                city.id = int.Parse(reader["ID"].ToString());
+                city.cityName = reader["CityName"].ToString();
+                city.about = reader["About"].ToString();
+                city.country = reader["Country"].ToString();
+                cityList.Add(city);
+            }
+            reader.Close();
+            connection.Close();
+            LoadShowListView(cityList);
+        }
+
+        public void LoadShowListView(List<City> citiyList)
+        {
+            
+            int i = 1;
+            showListView.Items.Clear();
+            foreach (var city in citiyList)
+            {
+                ListViewItem item = new ListViewItem(city.id.ToString());
+                item.SubItems.Add(i.ToString());
+                item.SubItems.Add(city.cityName);
+                item.SubItems.Add(city.about);
+                item.SubItems.Add(city.country);
+                showListView.Items.Add(item);
+                i = i + 1;
+            }
+
+        }
+
+        private void showListView_DoubleClick_1(object sender, EventArgs e)
+        {
+            ListViewItem item = showListView.SelectedItems[0];
+
+            int id = int.Parse(item.Text.ToString());
+
+            City city = GetCityById(id);
+            if (city != null)
+            {
+                isUpdateMode = true;
+                saveButton.Text = "Update";
+                cityId = city.id;
+                cityNameTextBox.ReadOnly = true;
+                cityNameTextBox.Text = city.cityName;
+                aboutTextBox.Text = city.about;
+                countryTextBox.Text = city.country;
+            }
+        }
+
+        public City GetCityById(int id)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            string selectQuery = "SELECT * FROM cityInfoTable WHERE ID='"+id+"'";
+            SqlCommand command = new SqlCommand(selectQuery, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+
+            List<City> cityList = new List<City>();
+
+            while (reader.Read())
+            {
+                City city = new City();
+                city.id = int.Parse(reader["ID"].ToString());
+                city.cityName = reader["CityName"].ToString();
+                city.about = reader["About"].ToString();
+                city.country = reader["Country"].ToString();
+                cityList.Add(city);
+            }
+            reader.Close();
+            connection.Close();
+            return cityList.FirstOrDefault();
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            string search = searchTextBox.Text;
+            SearchAllData(search);
+        }
+
+        public void SearchAllData(string byCity)
+        {
+            if (searchByCityRadioButton.Checked)
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+
+                string selectQuery = "SELECT * FROM cityInfoTable WHERE CityName LIKE'%" + byCity + "%'";
+
+                SqlCommand command = new SqlCommand(selectQuery, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                List<City> cityList = new List<City>();
+
+                while (reader.Read())
+                {
+                    City city = new City();
+                    city.id = int.Parse(reader["ID"].ToString());
+                    city.cityName = reader["CityName"].ToString();
+                    city.about = reader["About"].ToString();
+                    city.country = reader["Country"].ToString();
+                    cityList.Add(city);
+                }
+                reader.Close();
+                connection.Close();
+                LoadShowListView(cityList);
+            }
+            else
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+
+                string selectQuery = "SELECT * FROM cityInfoTable WHERE Country LIKE'%" + byCity + "%'";
+
+                SqlCommand command = new SqlCommand(selectQuery, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                List<City> cityList = new List<City>();
+
+                while (reader.Read())
+                {
+                    City city = new City();
+                    city.id = int.Parse(reader["ID"].ToString());
+                    city.cityName = reader["CityName"].ToString();
+                    city.about = reader["About"].ToString();
+                    city.country = reader["Country"].ToString();
+                    cityList.Add(city);
+                }
+                reader.Close();
+                connection.Close();
+                LoadShowListView(cityList);
+            }
+        }
+       
+    }
+}
